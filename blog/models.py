3b7dea3fa9ff510c2c5f9a9eb4bf65c2e631624b
WaliEEE <jocularriad2@gmail.com>
2020-09-01 09:58:01 +0100

    Static Files Added

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,12 @@ class Blog(models.Model):
 	image   = models.ImageField(upload_to='images/', null=True)
 	summary = models.TextField(max_length=5000, null=True)
 
-	
+
+	def __str__(self):
+		return self.title
+
+	def body(self):
+		return self.summary[:100]
+
+	def date(self):
+		return self.pub_date.strftime('%b %e , %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
 def allblogs(request):
+    new_blogs = Blog.objects
+    return render(request, 'blog/allblogs.html', {'collections':new_blogs})
 
-	new_blogs = Blog.objects
 
-	return render(request, 'blog/allblogs.html', {'collections':new_blogs})
+def detail(request, blog_id):
+    dblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/sblog.html', {'single':dblog})
+
